54b3ca6d85a3945a06ed6da0c7b5a82337dd739e
robzgf <rgrielfilho@gmail.com>
2019-09-17 22:41:46 -0300

    Ajuste de Endereço

--- cart.php
+++ cart.php
@@ -83,14 +83,106 @@ $app->get("/checkout", function(){
 	
 	User::verifyLogin(false);
 	
-	$cart = Cart::getFromSession();
-	
 	$address = new Address();
 	
+	$cart = Cart::getFromSession();
+	
+/*	if (isset($_GET['zipcode'])) {
+		$_GET['zipcode'] = $cart->deszipcode();
+	}*/
+	
+	if (isset($_GET['zipcode'])) {
+		
+		$address->loadFromCEP($_GET['zipcode']);
+		
+		$cart->setdeszipcode($_GET['zipcode']);
+		
+		$cart->save();
+		
+		$cart->getCalculateTotal();
+		
+	}
+	
+	if (!$address->getdesaddress()) $address->setdesaddress('');
+	if (!$address->getdesnumber()) $address->setdesnumber('');
+	if (!$address->getdescomplement()) $address->setdescomplement('');
+	if (!$address->getdesdistrict()) $address->setdesdistrict('');
+	if (!$address->getdescity()) $address->setdescity('');
+	if (!$address->getdesstate()) $address->setdesstate('');
+	if (!$address->getdescountry()) $address->setdescountry('');
+	if (!$address->getdeszipcode()) $address->setdeszipcode('');
+		
 	$page = new Page();
 	
 	$page->setTpl("checkout", [
 		'cart'=>$cart->getValues(),
-		'address'=>$address->getValues()
+		'address'=>$address->getValues(),
+		'products'=>$cart->getProducts(),
+		'error'=>Address::getMsgError()
 	]);
+});
+
+$app->post("/checkout", function(){
+	
+	User::verifyLogin(false);
+	
+	if (!isset($_POST['zipcode']) || $_POST['zipcode'] === '') {
+		
+		Address::setMsgError("Por favor, preencha o CEP");
+		header("Location: /checkout");
+		exit;
+	}
+	
+	if (!isset($_POST['desaddress']) || $_POST['desaddress'] === '') {
+		
+		Address::setMsgError("Por favor, preencha o endereço");
+		header("Location: /checkout");
+		exit;
+	}
+	if (!isset($_POST['desnumber']) || $_POST['desnumber'] === '') {
+		
+		Address::setMsgError("Por favor, preencha o número");
+		header("Location: /checkout");
+		exit;
+	}
+	if (!isset($_POST['desdistrict']) || $_POST['desdistrict'] === '') {
+		
+		Address::setMsgError("Por favor, preencha o bairro");
+		header("Location: /checkout");
+		exit;
+	}
+	if (!isset($_POST['descity']) || $_POST['descity'] === '') {
+		
+		Address::setMsgError("Por favor, preencha a cidade");
+		header("Location: /checkout");
+		exit;
+	}
+	if (!isset($_POST['desstate']) || $_POST['desstate'] === '') {
+		
+		Address::setMsgError("Por favor, preencha o estado");
+		header("Location: /checkout");
+		exit;
+	}
+	if (!isset($_POST['descountry']) || $_POST['descountry'] === '') {
+		
+		Address::setMsgError("Por favor, preencha o país");
+		header("Location: /checkout");
+		exit;
+	}
+	
+	
+	$user = User::getFromSession();
+	
+	$address = new Address();
+	
+	$_POST['deszipcode'] = $_POST['zipcode'];
+	$_POST['idperson'] = $user->getidperson();
+	$address->setData($_POST);
+	
+	$address->save();
+	
+	header("Location: /order");
+	exit;
+	
+	
 });
--- vendor/hcodebr/php-classes/src/Model/Address.php
+++ vendor/hcodebr/php-classes/src/Model/Address.php
@@ -7,5 +7,159 @@ use \Hcode\Model;
 
 class Address extends Model 
 {
+	const SESSION = "Address";
+	const SESSION_ERROR = "AddressError";
+	
+	public static function getCEP($nrcep)
+	{
+		$nrcep = str_replace("-", "", $nrcep);
+		
+		$ch = curl_init();
+		
+		curl_setopt($ch, CURLOPT_URL, "https://viacep.com.br/ws/$nrcep/json/");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		
+		$data = json_decode(curl_exec($ch), true);
+		
+		curl_close($ch);
+		
+		return $data;
+	}
+	
+	public function loadFromCEP($nrcep)
+	{
+		
+		$data = Address::getCEP($nrcep);
+		
+		if (isset($data['logradouro']) && $data['logradouro']) {
+		
+			$this->setdesaddress($data['logradouro']);
+			$this->setdescomplement($data['complemento']);
+			$this->setdesdistrict($data['bairro']);
+			$this->setdescity($data['localidade']);
+			$this->setdesstate($data['uf']);
+			$this->setdescountry('Brasil');
+			$this->setdeszipcode($nrcep);
+		
+		}
+		
+	}
+	
+	public function save()
+	{
+		$sql = new Sql();
+		$results = $sql->select("CALL sp_addresses_save(:idaddress, :idperson, :desaddress, :desnumber, :descomplement, :descity, :desstate, :descountry, :deszipcode, :desdistrict)", [
+			':idaddress'=>$this->getidaddress(),
+			':idperson'=>$this->getidperson(),
+			':desaddress'=>utf8_decode($this->getdesaddress()),
+			':desnumber'=>$this->getdesnumber(),
+			':descomplement'=>utf8_decode($this->getdescomplement()),
+			':descity'=>utf8_decode($this->getdescity()),
+			':desstate'=>utf8_decode($this->getdesstate()),
+			':descountry'=>utf8_decode($this->getdescountry()),
+			':deszipcode'=>$this->getdeszipcode(),
+			':desdistrict'=>$this->getdesdistrict()
+		]);
+		
+		
+		if (count($results) > 0) {
+			$this->setData($results[0]);
+		}
+	}
+	
+	public static function setMsgError($msg)
+	{
+		$_SESSION[Address::SESSION_ERROR] = $msg;
+	}
+	
+	public static function getMsgError()
+	{
+		$msg = (isset($_SESSION[Address::SESSION_ERROR])) ? $_SESSION[Address::SESSION_ERROR] : "";
+		
+		Address::clearMsgError();
+		
+		return $msg;
+	}
+	
+	public static function clearMsgError()
+	{
+		$_SESSION[Address::SESSION_ERROR] = NULL;
+	}
+	
+	public static function setError($msg)
+	{
+
+		$_SESSION[Address::ERROR] = $msg;
+
+	}
+
+	public static function getError()
+	{
+
+		$msg = (isset($_SESSION[Address::ERROR]) && $_SESSION[Address::ERROR]) ? $_SESSION[Address::ERROR] : '';
+
+		Address::clearError();
+
+		return $msg;
+
+	}
+
+	public static function clearError()
+	{
+
+		$_SESSION[Address::ERROR] = NULL;
+
+	}
+
+	public static function setSuccess($msg)
+	{
+
+		$_SESSION[Address::SUCCESS] = $msg;
+
+	}
+
+	public static function getSuccess()
+	{
+
+		$msg = (isset($_SESSION[Address::SUCCESS]) && $_SESSION[Address::SUCCESS]) ? $_SESSION[Address::SUCCESS] : '';
+
+		Address::clearSuccess();
+
+		return $msg;
+
+	}
+
+	public static function clearSuccess()
+	{
+
+		$_SESSION[Address::SUCCESS] = NULL;
+
+	}
+
+	public static function setErrorRegister($msg)
+	{
+
+		$_SESSION[Address::ERROR_REGISTER] = $msg;
+
+	}
+
+	public static function getErrorRegister()
+	{
+
+		$msg = (isset($_SESSION[Address::ERROR_REGISTER]) && $_SESSION[Address::ERROR_REGISTER]) ? $_SESSION[Address::ERROR_REGISTER] : '';
+
+		Address::clearErrorRegister();
+
+		return $msg;
+
+	}
+
+	public static function clearErrorRegister()
+	{
+
+		$_SESSION[Address::ERROR_REGISTER] = NULL;
+
+	}
 	
 }
